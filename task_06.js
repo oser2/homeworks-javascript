706bf808b19d3c35ce0903b5de215e5abe87fd28
o_ser <ostapius.2009@gmail.com>
2024-11-22 19:27:32 +0200

    a

--- task_06.js
+++ task_06.js
@@ -1,8 +1,25 @@
 let results = document.querySelectorAll(".result")
 
-//
+//1
+let arr = []
 let i = 0
 while(i < 10){
     i++
+    arr.push(i)
 }
-results[0].textContent = i
+results[0].textContent = arr
+
+//2
+arr = []
+for(let i = 1; i < 21; i++){
+    if(i % 2 != 0){
+         continue   
+    }
+    else{
+        arr.push(i)
+    }
+
+}   
+results[1].textContent = arr
+
+//
